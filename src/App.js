dff7b752d5dc560f0b3feeba5191026f4ebc88ec
Steven Tohme <steventohme59@gmail.com>
2023-04-04 21:15:55 -0400

    Model zooms to fit screen on click

--- src/App.js
+++ src/App.js
@@ -18,14 +18,16 @@ function Model({ open, hinge, ...props }) {
 
   useFrame((state) => {
     const t = state.clock.getElapsedTime()
-    group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, open ? Math.cos(t / 10) / 10 + 0.25 : 0, 0.1)
-    /*group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, open ? Math.sin(t / 10) / 4 : 0, 0.1) */
-    /*group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, open ? Math.sin(t / 10) / 10 : 0, 0.1)*/
-    
-    group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, open ? -3 : -4, 0.05)
+    group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, open ? 0.4 : 0, 0.1)
+    /*group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, open ? Math.cos(t / 10) / 10 + 0.25 : 0, 0.1)*/
 
+    group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, open ? -2.8 : -4.3, 0.1)
+    group.current.position.x = THREE.MathUtils.lerp(group.current.position.x, open ? -0.1 : 0, 0.1)
+    /*group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, open ? (-2 + Math.sin(t)) / 3 : -4.3, 0.1)*/
+    /* perfect positions, -2.8 y, 24.1 z, 0.4x */
     /* move the model towards the camera */
-    group.current.position.z = THREE.MathUtils.lerp(group.current.position.z, open ? t * 5 : -4.3, 0.1)
+    group.current.position.z = THREE.MathUtils.lerp(group.current.position.z, open ? 24.1 : 0,0.1)
+    
   })
 
   return (
@@ -33,21 +35,21 @@ function Model({ open, hinge, ...props }) {
       ref={group}
       {...props}
       onPointerOver={(e) => (e.stopPropagation(), setHovered(true))}
-      onPointerOut={(e) => setHovered(false)}
+      onPointerOut={() => setHovered(false)}
       dispose={null}>
-      <three.group rotation-x={hinge} position={[0, -0.04, 0.41]}>
+      <three.group rotation-x={hinge} position={[0.1, -0.04, 0.41]}>
         <group position={[0, 2.96, -0.13]} rotation={[Math.PI / 2, 0, 0]}>
           <mesh material={materials.aluminium} geometry={nodes['Cube008'].geometry} />
           <mesh material={materials['matte.001']} geometry={nodes['Cube008_1'].geometry} />
           <mesh material={materials['screen.001']} geometry={nodes['Cube008_2'].geometry} />
         </group>
       </three.group>
-      <mesh material={materials.keys} geometry={nodes.keyboard.geometry} position={[1.79, 0, 3.45]} />
-      <group position={[0, -0.1, 3.39]}>
+      <mesh material={materials.keys} geometry={nodes.keyboard.geometry} position={[1.9, 0, 3.45]} />
+      <group position={[0.1, -0.1, 3.39]}>
         <mesh material={materials.aluminium} geometry={nodes['Cube002'].geometry} />
         <mesh material={materials.trackpad} geometry={nodes['Cube002_1'].geometry} />
       </group>
-      <mesh material={materials.touchbar} geometry={nodes.touchbar.geometry} position={[0, -0.03, 1.2]} />
+      <mesh material={materials.touchbar} geometry={nodes.touchbar.geometry} position={[0.1, -0.03, 1.2]} />
     </group>
   )
 }
@@ -56,6 +58,7 @@ function Model({ open, hinge, ...props }) {
 export default function App() {
   const [open, setOpen] = useState(false)
   const props = useSpring({ open: Number(open) })
+
   return (
     <web.main style={{ background: props.open.to([0, 1], ['#f0f0f0', '#ffab87']) }}>
       <web.h1 style={{ opacity: props.open.to([0, 1], [1, 0]), transform: props.open.to((o) => `translate3d(-50%,${o * 50 - 100}px,0)`) }}>
