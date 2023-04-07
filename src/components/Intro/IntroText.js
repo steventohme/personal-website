5035b1f6b78f2ca37e92b67add413d0a77c5457e
Steven Tohme <steventohme59@gmail.com>
2023-04-07 12:40:35 -0400

    added image, for some reason my mug isnt showing

--- src/components/Intro/IntroText.js
+++ src/components/Intro/IntroText.js
@@ -1,17 +1,19 @@
 
 import './Intro.css'
+import me from '../../Images/me.png'
 export default function IntroText(){
     return (
         <div>
-        <intro-title>
-            Why, hello there! I’m Steven. 
-        </intro-title>
-        
-        <intro-text>
-            I’m a self-proclaimed minesweeper master. I’m 
-            just beginning my journey in tech and am excited to get my feet wet. 
-            Click around to find out more about me!
-        </intro-text>
+            <me-img/>
+            <intro-title>
+                Why, hello there! I’m Steven. 
+            </intro-title>
+            
+            <intro-text>
+                I’m a self-proclaimed minesweeper master. I’m 
+                just beginning my journey in tech and am excited to get my feet wet. 
+                Click around to find out more about me!
+            </intro-text>
         </div>
 
     )
