40abc494386e60b48ad69af057d40fc4dae73178
Steven Tohme <steventohme59@gmail.com>
2023-04-09 16:28:47 -0400

    removed orbit controls

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,7 +37,6 @@ export default function App() {
           <Environment preset="city" />
         </Suspense>
         <ContactShadows position={[0, -4.5, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} />
-        <OrbitControls/>
       </Canvas>
     </web.main>
   )
