adb3c11cc81a2204a48962e8d6aab112da208365
Steven Tohme <steventohme59@gmail.com>
2023-04-15 15:26:58 -0400

    added linkendin logo

--- src/components/Intro/SocialMediaButtons.js
+++ src/components/Intro/SocialMediaButtons.js
@@ -9,10 +9,18 @@ export default function SocialMediaButtons(){
     return (
         <Center h={1600}>
             <Grid justify="center" align="center" grow>
-                <Grid.Col >
+                <Grid.Col span={1}>
                     <a href="https://www.instagram.com/stevenlikessandwiches/" target="_blank" rel="external noreferrer">
                         <ActionIcon size="xl" color="dark">
                             <AiFillInstagram size={100} />
+                        </ActionIcon>
+                    </a>
+                </Grid.Col>
+            
+                <Grid.Col span={1}>
+                    <a href="https://linkedin.com/in/steven-tohme" target="_blank" rel="external noreferrer">
+                        <ActionIcon size="xl" color="dark">
+                            <AiFillLinkedin size={100} />
                         </ActionIcon>
                     </a>
                 </Grid.Col>
