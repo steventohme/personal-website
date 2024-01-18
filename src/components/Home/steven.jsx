a45edda83e5d73088af4e0a897ac57dd67e32e67
Steven Tohme <steventohme59@gmail.com>
2024-01-17 19:59:47 -0500

    add elevenlabs mapping for visemes

--- src/components/Home/steven.jsx
+++ src/components/Home/steven.jsx
@@ -82,6 +82,18 @@ const facialExpressions = {
   },
 };
 
+const corresponding = {
+  A: "viseme_PP",
+  B: "viseme_kk",
+  C: "viseme_I",
+  D: "viseme_AA",
+  E: "viseme_O",
+  F: "viseme_U",
+  G: "viseme_FF",
+  H: "viseme_TH",
+  X: "viseme_PP",
+};
+
 export function Steven(props) {
   const { nodes, materials } = useGLTF("/steven.glb");
 
@@ -117,6 +129,7 @@ export function Steven(props) {
     });
   };
 
+  
   return (
     <group {...props} dispose={null}>
       <primitive object={nodes.Hips} />
