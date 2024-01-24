0871025ee759dcf9121283abc5c6bc4d95d0289a
Steven Tohme <steventohme59@gmail.com>
2024-01-24 14:46:31 -0500

    import cors and express

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
 import { promises} from "fs";
+import cors from "cors";
+import express from "express";
 dotenv.config();
 
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
