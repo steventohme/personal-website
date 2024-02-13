aaf260f91351ca43ebc66ebe39076fe5820a4d16
Steven Tohme <steventohme59@gmail.com>
2024-02-12 20:25:29 -0500

    add backend url

--- frontend/src/hooks/useChat.jsx
+++ frontend/src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "http://localhost:3000";
+const backendUrl = process.env.BACKEND_URL || "http://localhost:3000";
 
 const ChatContext = createContext();
 
