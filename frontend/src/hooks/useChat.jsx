b40ebe3439e6c764e798c465333c109c88935ca6
Steven Tohme <steventohme59@gmail.com>
2024-02-12 19:32:48 -0500

    fix typo

--- frontend/src/hooks/useChat.jsx
+++ frontend/src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = proccess.env.BACKEND || "http://localhost:3000";
+const backendUrl = process.env.BACKEND || "http://localhost:3000";
 
 const ChatContext = createContext();
 
