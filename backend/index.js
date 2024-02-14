93ae9a20864578529c247e58d8659074f854c5de
Steven Tohme <steventohme59@gmail.com>
2024-02-13 19:51:49 -0500

    still play audio

--- backend/index.js
+++ backend/index.js
@@ -99,7 +99,7 @@ app.post("/chat", async (req, res) => {
       await voice.textToSpeech(elevenLabsApiKey, voiceID, fileName, textInput, 0.5, 0.75);
       // generate lipsync
       //await lipSyncMessage(i);
-      //message.audio = await audioFileToBase64(fileName);
+      message.audio = await audioFileToBase64(fileName);
       //message.lipsync = await readJsonTranscript(`audios/message_${i}.json`);
     }
     
