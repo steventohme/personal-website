8295973f416cdf357d3d594bf8b1f4aa1bc2d845
Steven Tohme <steventohme59@gmail.com>
2024-06-16 15:56:32 -0400

    initial commit

--- frontend/src/hooks/useChat.jsx
+++ frontend/src/hooks/useChat.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const backendUrl = window.location.hostname === 'localhost' 
 ? 'http://localhost:3000' 
-: 'personal-website-backend';
+: 'http://d48ggk0.165.227.218.168.sslip.io';
 
 const ChatContext = createContext();
 
