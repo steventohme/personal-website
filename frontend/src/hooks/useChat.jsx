c8eebcebac3c75f3f7a36161bb5f733e76219f5e
Steven Tohme <steventohme59@gmail.com>
2024-06-16 17:33:53 -0400

    try something

--- frontend/src/hooks/useChat.jsx
+++ frontend/src/hooks/useChat.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const backendUrl = window.location.hostname === 'localhost' 
 ? 'http://localhost:3000' 
-: 'https://d48ggk0.165.227.218.168.sslip.io';
+: 'http://d48ggk0.165.227.218.168.sslip.io';
 
 
 
@@ -17,7 +17,7 @@ export const ChatProvider = ({ children }) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": *,
+          "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify({ message }),
       });
